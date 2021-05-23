fd540eeba3a42a053c8994e6228e9b33edfafbe0
yu_h-neu <yu_h-neu@reachauto.com>
2021-05-23 20:10:31 +0800

    参数

--- src/network/detail.js
+++ src/network/detail.js
@@ -9,3 +9,29 @@ export function getDetail(iid) {
     },
   });
 }
+
+export class Goods {
+  constructor(itemInfo, columns, services) {
+    this.title = itemInfo.title;
+    this.desc = itemInfo.desc;
+    this.oldPrice = itemInfo.oldPrice;
+    this.lowNowPrice = itemInfo.lowNowPrice;
+    this.price = itemInfo.price;
+    this.discountBgColor = itemInfo.discountBgColor;
+    this.discountDesc = itemInfo.discountDesc;
+
+    this.columns = columns;
+
+    this.services = services;
+  }
+}
+
+export class ShopInfo {
+  constructor(shopInfo) {
+    this.logo = shopInfo.shopLogo;
+    this.score = shopInfo.score;
+    this.goods = shopInfo.cGoods;
+    this.sells = shopInfo.cSells;
+    this.name = shopInfo.name;
+  }
+}
--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -2,14 +2,18 @@
   <div>
     <detail-nav-bar/>
     <detail-swiper :top-images="topImages"/>
+    <detail-goods :goods="goods"/>
+    <detail-shop-info :shop="shopInfo"/>
   </div>
 </template>
 
 <script>
 import DetailNavBar from './childComps/DetailNavBar.vue';
 import DetailSwiper from './childComps/DetailSwiper.vue';
+import DetailShopInfo from './childComps/DetailShopInfo.vue';
 
-import { getDetail } from 'network/detail.js';
+import { getDetail, Goods, ShopInfo} from 'network/detail.js';
+import DetailGoods from './childComps/DetailGoods';
 
 export default {
   name: 'Detail',
@@ -17,25 +21,31 @@ export default {
     return {
       id: '',
       topImages: [],
+      goods: [],
+      shopInfo: {},
     };
   },
   created() {
     this.id = this.$route.params.id;
-    this.getDetail()
-      .then(res => {
-        console.log(res);
-        let result = res.result;
-        this.topImages = result.itemInfo.topImages;
-      });
+    this.getDetail();
   },
   components: {
+    DetailGoods,
     DetailNavBar,
     DetailSwiper,
+    DetailShopInfo,
   },
   methods: {
     getDetail() {
-      return getDetail(this.id);
+      return getDetail(this.id)
+        .then((res) => {
+          let result = res.result;
+          this.topImages = result.itemInfo.topImages;
+          this.goods = new Goods(result.itemInfo, result.columns, result.shopInfo.services);
+          this.shopInfo = new ShopInfo(result.shopInfo);
+        });
     },
+
   },
 };
 </script>
